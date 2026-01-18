c368e0dfd0d5b2540350d798d2ab6c0733837387
PyGuy1 <153994805+PyGuy1@users.noreply.github.com>
2026-01-18 22:35:36 +0530

    Update service worker for caching and fetch logic
    Refactor service worker to use precache assets and improve fetch handling.

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,52 @@
 const CACHE_NAME = "todo-pwa-v1";
-const ASSETS = [
+
+const PRECACHE_ASSETS = [
   "./",
   "./app.html",
-  "./ToDo.png",
   "./manifest.json",
+  "./ToDo.png",
   "./static/css/tailwind.css",
   "./static/js/main.4a82c912.js"
 ];
 
-// Install
+// INSTALL
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE_ASSETS))
   );
 });
 
-// Fetch
+// ACTIVATE
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// FETCH
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    fetch(event.request)
+      .then(res => {
+        if (
+          res.status === 200 &&
+          event.request.url.startsWith(self.location.origin)
+        ) {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(cache =>
+            cache.put(event.request, clone)
+          );
+        }
+        return res;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
